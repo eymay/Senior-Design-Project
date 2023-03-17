82c59eb1514766bfa7a657810cba42b1f45fd7ee
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-03-17 10:32:50 +0300

    Return type voided to clear the ABI Stores

--- s-box/archive/s-box-reg.c
+++ s-box/archive/s-box-reg.c
@@ -4,7 +4,7 @@ typedef struct {
 
 
 
-ascon_state_t sbox(ascon_state_t state) {
+void sbox(ascon_state_t state) {
      int t0, t1, t2, t3, t4;
 state.x[0] ^= state.x[4];    state.x[4] ^= state.x[3];    state.x[2] ^= state.x[1];
 t0  = state.x[0];    t1  = state.x[1];    t2  = state.x[2];    t3  = state.x[3];    t4  = state.x[4];
@@ -13,6 +13,6 @@ t0 &= state.x[1];    t1 &= state.x[2];    t2 &= state.x[3];    t3 &= state.x[4];
 state.x[0] ^= t1;    state.x[1] ^= t2;    state.x[2] ^= t3;    state.x[3] ^= t4;    state.x[4] ^= t0;
 state.x[1] ^= state.x[0];    state.x[0] ^= state.x[4];    state.x[3] ^= state.x[2];    state.x[2] =~ state.x[2];
 
-return state;
+return;
 }
 
